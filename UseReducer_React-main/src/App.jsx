ae7825a4de36ea4b7e2ee8f871e74427bcae3d4c
KevinLavigne <lavigne.kevin02@gmail.com>
2022-06-02 11:20:38 +0200

    workshop useReducer: creation d'une toDo list editable via useReducer you can add delete modify his state to done or in pause all of this via reducer

---
+++ UseReducer_React-main/src/App.jsx
@@ -0,0 +1,43 @@
+import react, { useState, useReducer } from 'react';
+import tasks from './data/tasks';
+import './App.css';
+import ToDoForm from './components/ToDoForm';
+import ToDoCard from './components/ToDoCard';
+
+function App() {
+	const todoReducer = (state, action) => {
+		console.log(state);
+		console.log(action);
+		switch (action.type) {
+			case 'ADD_TODO':
+				return [...state, action.payload];
+			case 'REMOVE_TODO':
+				return state.filter(
+					(todo) =>
+						todo.label !== action.payload.label &&
+						todo.description !== action.payload.description
+				);
+			case 'ADD_CLS':
+				const newToDo = [...state];
+				const index = newToDo.indexOf(action.payload);
+				newToDo[index].cls = action.cls;
+				return newToDo;
+			default:
+				return state;
+		}
+	};
+	const [todos, dispatch] = useReducer(todoReducer, tasks);
+	return (
+		<div className="App">
+			<h1 className="text-center">My To Do List App for better learning</h1>
+			<ToDoForm dispatch={dispatch} />
+			<div className="container">
+				{todos.map((task) => (
+					<ToDoCard task={task} key={task.label} dispatch={dispatch} />
+				))}
+			</div>
+		</div>
+	);
+}
+
+export default App;
---
+++ UseReducer_React-main/src/components/ToDoCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BiTrash, BiPause } from 'react-icons/bi';
+import { MdDone } from 'react-icons/md';
+
+const ToDoCard = ({ task, dispatch }) => {
+	const dynamicCls = `row m-4 border shadow rounded ${task.cls}`;
+	return (
+		<div className={dynamicCls}>
+			<div className="col-2">{task.label}</div>
+			<div className="col-4">{task.description}</div>
+			<div className="col-2">{task.type}</div>
+			<div className="col-2">{task.difficulty}</div>
+			<div className="col-2">
+				<MdDone
+					onClick={() =>
+						dispatch({ type: 'ADD_CLS', payload: task, cls: 'text-success' })
+					}
+				/>
+				<BiPause
+					onClick={() =>
+						dispatch({ type: 'ADD_CLS', payload: task, cls: 'text-warning' })
+					}
+				/>
+				<BiTrash
+					onClick={() => dispatch({ type: 'REMOVE_TODO', payload: task })}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default ToDoCard;
